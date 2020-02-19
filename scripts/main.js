c32067e41c6cfee9615c734eeef2d39ffc6e26a8
Michal Pillar <michal.pillar@gmail.com>
2020-02-18 23:34:53 -0800

    Created JS DOM selectors

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+const emailField = document.getElementById('email');
+const submitBtn = document.getElementById('submit');
+const errorTxt = document.getElementById('error-text');
+const form = document.querySelector('form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+});
